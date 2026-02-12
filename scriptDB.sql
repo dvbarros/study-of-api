483bee0e4abdd3390d510059865458cab6a1a354
Davi Barros <163138279+dvbarros@users.noreply.github.com>
2026-02-12 17:04:07 -0300

    Delete scriptDB.sql

--- scriptDB.sql
+++
@@ -1,9 +0,0 @@
-
-
-CREATE TABLE IF NOT EXISTS usersTest (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT,
-      email TEXT
-    );
-
-INSERT INTO usersTest (name, email) VALUES ('John Doe', 'john.doe@example.com');
